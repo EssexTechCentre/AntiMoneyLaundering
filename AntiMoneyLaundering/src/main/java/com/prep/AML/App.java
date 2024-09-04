7356e98dc0314e299d1954d882b97c752fe24e33
Karl <karl.sherwin@gmail.com>
2024-09-04 07:56:10 +0100

    Update App.java

--- AntiMoneyLaundering/src/main/java/com/prep/AML/App.java
+++ AntiMoneyLaundering/src/main/java/com/prep/AML/App.java
@@ -12,7 +12,7 @@ public class App {
 	//(1)  Please see src/main/doc "aml design.PNG" for task design 
 	//(2)  To run executable (args[0] is location of your AML.CVS file) - 
 	//			java -jar AntiMoneyLaundering-0.0.1.jar C:\User\Temp
-	//(3)  Assumed Java 8 for RBC compatibility.
+	//(3)  Assumed Java 8 for compatibility.
 	//(4)  Core java only used, no external third party loggers, data structures etc 
 	//(5)  Running as an executable one can pass in a directory to read AML files from
 	
